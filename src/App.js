43509ee464acb315c84984972c736970bdf8a68b
HaIvanVillegas <alberto_ha@live.com>
2020-03-03 22:55:07 -0600

    added routes withRoute and Switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Homepage from "./pages/homepage/homepage.component";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+const Hatspage = () => (
+  <div>
+    <h1>Hatspage!</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/hats" component={Hatspage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{
@@ -12,10 +16,10 @@ const MenuItem = ({ title, imageUrl, size }) => {
       ></div>
       <div className="content">
         <h1 className="title"> {title.toUpperCase()}</h1>
-        <span className="subtitle">Shop now!</span>
+        <span className="subtitle">Shop now</span>
       </div>
     </div>
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
