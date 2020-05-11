78875c690c67388603d2271934a81da24fa10963
HaIvanVillegas <alberto_ha@live.com>
2020-05-11 09:57:22 -0500

    added backend and stripe payment

--- src/components/stripe-button/stripe-buton.component.jsx
+++ src/components/stripe-button/stripe-buton.component.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_XvZWqGRM0wovvsGTVmxAe1OK00fMUnFpEt";
 
-  const onToken = token => {
-    console.log(token);
-    alert("Payment successful");
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((res) => {
+        alert("Payment Successful!");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There war an issue with your payment. Please use the provided credit card."
+        );
+      });
   };
 
   return (
